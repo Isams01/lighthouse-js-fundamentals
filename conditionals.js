19f1d682a4445547ba2a4c2ca0fa266cfc855276
Isamu Ito <isamsito@gmail.com>
2021-01-10 00:49:34 +0000

    more examples added to conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,27 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+
+
+// other conditional examples
+const isCitizen = true;
+const age = 26;
+
+// 'and' operator
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+// 'or' operator
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+// "isnot" operator?
+const raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
